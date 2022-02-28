111a5cd787c6ce83976456a9bc4764035642cdc7
salmancz <salman.jsdev@gmail.com>
2022-02-28 22:23:41 +0530

    training

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,7 @@
 import "./App.css";
-import Todo from "./components/Todo";
-import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header logo="SuperStar" />
-      <Header logo="imran" />
-      <Header logo="dasd av" />
-      <Header logo="saddsadas" />
-      <Todo />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
